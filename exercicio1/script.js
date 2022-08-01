e7e749fbd6bbee00e6bd3fef6750e244d5ed8fc9
falacomomenna <danielmenna@gmail.com>
2022-08-01 18:16:55 -0300

    resolução exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+const numeros = [1, 3, 5, 7, 9]
+console.log(numeros)
+
+const nomes = ['Daniel', 'João', 'Paulo']
+console.log(nomes)
+
+const identidade = [8091393499, 'Daniel Ribeiro Menna', true]
+console.log(identidade)
