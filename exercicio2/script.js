a10a912a428f670b4d0cdd4a3eb2fbb628c92173
falacomomenna <danielmenna@gmail.com>
2022-08-01 18:32:16 -0300

    resolução exercício 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,18 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const numeros = [1, 3, 5, 7, 9]
+console.log(numeros.length)
+
+const nomes = ['Daniel', 'João', 'Paulo']
+console.log(nomes.length)
+
+const identidade = [8091393499, 'Daniel Ribeiro Menna', true]
+console.log(identidade.length)
+
+console.log(numeros[0])
+console.log(nomes[1])
+console.log(identidade[2])
+
+numeros.unshift(0)
+console.log(numeros.includes(0))
+
+identidade.push('Venancio Aires')
+console.log(identidade.includes('Porto Alegre'))
